40903f560d3bbc9cea56c3ecb70f8f8608bb77ca
Rahul <rahukand@gmail.com>
2021-11-13 12:17:30 -0800

    Created DayLog class
    This class will model all the logs on a given day.

---
+++ team-04-08/src/application/model/DayLog.java
@@ -0,0 +1,12 @@
+package application.model;
+
+import java.util.TreeMap;
+
+public class DayLog extends TreeMap<String, Log> {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+}
