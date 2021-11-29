645e8ae44032bbe8041143a8a30e4f5a8484849a
Zariphron <90808376+Zariphron@users.noreply.github.com>
2021-11-29 15:55:04 -0800

    Altered to swap between buttons
    When start is called it will now be replaced with a "stop" button, and when that is called it will be replaced with a "resume" button.

--- team-04-08/src/application/control/Widget3Controller.java
+++ team-04-08/src/application/control/Widget3Controller.java
@@ -15,10 +15,12 @@ public class Widget3Controller extends WidgetController {
 	
 	@FXML Label timeDisplay;
 	@FXML Button startBtn;
-	@FXML Button pauseBtn;
+	@FXML Button resumeBtn;
+	@FXML Button stopBtn;
 	private int currentTimeElapsed = 0;
 	private int minuteDisplay;
 	private double startTime;
+	private double resumeTime;
 	private double endTime;
 
 	private Logger2 peerMentorLogger;
@@ -29,17 +31,19 @@ public class Widget3Controller extends WidgetController {
 		ObservableList<String> validActivities = FXCollections.observableList(peerMentorLogger.getValidActivities());
 		dropDownBox.setItems(validActivities);
 		timeDisplay.setText("" + 0);
-		pauseBtn.setDisable(true);
+		resumeBtn.setVisible(false);
+		stopBtn.setVisible(false);
 	}
 	
-	public void swapStartPauseStatus() {
-		if (pauseBtn.isDisabled()) {
-			pauseBtn.setDisable(false);
-			startBtn.setDisable(true);
+	// Sets the stop resume button to take the place of the stop button when pressed and vice versa. 
+	public void swapResumeStopStatus() {
+		if (stopBtn.isVisible()) {
+			stopBtn.setVisible(false);
+			resumeBtn.setVisible(true);
 		}
 		else {
-			startBtn.setDisable(false);
-			pauseBtn.setDisable(true);
+			resumeBtn.setVisible(false);
+			stopBtn.setVisible(true);
 		}
 	}
 	
@@ -49,19 +53,33 @@ public class Widget3Controller extends WidgetController {
 			System.out.println("No activity selected");
 			return;
 		}
-		swapStartPauseStatus();
+		startBtn.setVisible(false);
+		stopBtn.setVisible(true);
 		startTime = System.currentTimeMillis();
 	}
 	
+	/**
+	 * Calculates the time passed between pressing start, stop and resume.
+	 * 
+	 * Uses the start button's time once then switches to the resume button's time.
+	 */
 	public void timerCounting() {
-		currentTimeElapsed = currentTimeElapsed + (int)Math.ceil(endTime - startTime);
+		if (currentTimeElapsed == 0)
+			currentTimeElapsed = currentTimeElapsed + (int)Math.ceil(endTime - startTime);
+		else
+			currentTimeElapsed = currentTimeElapsed + (int)Math.ceil(endTime - resumeTime);
 		minuteDisplay = currentTimeElapsed/60000;
-		timeDisplay.setText("" + minuteDisplay + "min " + (currentTimeElapsed/1000) % 60 + "s");
+		timeDisplay.setText("" + minuteDisplay + " min");
+	}
+	
+	public void resumeTimer() {
+		swapResumeStopStatus();
+		resumeTime = System.currentTimeMillis();
 	}
 	
 	// Similar to the startTimer method except in reverse and gets a "cut off" point to calculate the total time elapsed.
-	public void pauseTimer() {
-		swapStartPauseStatus();
+	public void stopTimer() {
+		swapResumeStopStatus();
 		endTime = System.currentTimeMillis();
 		timerCounting();
 	}
@@ -74,28 +92,37 @@ public class Widget3Controller extends WidgetController {
 			return;
 		}
 		
-		if (minuteDisplay == 0) {
-			timerCounting();
-		}
-		
 		// Only allows users to log events that were at least one minute in length.
-		if (minuteDisplay <= 1) {
+		if (minuteDisplay < 1) {
 			System.out.println("Not enough time has passed to warrent logging");
 			return;
 		}
 		
+		// Refuses to allow users to log new events if the timer is still active.
+		if (stopBtn.isVisible()) {
+			System.out.println("Please stop the timer to log the appropriate amount of time passed.");
+			return;
+		}
 		
 		Log newLog = PeerMentorLog.createLog(dropDownBox.getValue(), minuteDisplay, commentBox.getText());
 		newLog.setLogDate(LocalDate.now());
 		if (newLog != null)
-			peerMentorLogger.add(newLog);
+			System.out.println("New log created: \n" + newLog);
+//			peerMentorLogger.add(newLog);
 		System.out.println("Recorded log:\n" + peerMentorLogger);
 		
 		dropDownBox.setValue(null);
+		commentBox.setText(null);
 		currentTimeElapsed = 0;
-		swapStartPauseStatus();
+		minuteDisplay = 0;
+		startTime = 0;
+		resumeTime = 0;
+		endTime = 0;
+		timeDisplay.setText("Timer");
+		resumeBtn.setVisible(false);
+		startBtn.setVisible(true);
 		
-		saveLogger(peerMentorLogger);
+//		saveLogger(peerMentorLogger);
 	}
 	
 	public void openOtherLoggerWidget() {
